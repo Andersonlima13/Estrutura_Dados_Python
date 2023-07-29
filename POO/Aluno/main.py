87016c9aa3894704b9ed05ebef46b2885e1468fd
unknown <anderson.lima18pb@gmail.com>
2023-07-28 22:24:28 -0300

    ED_PYTHON

---
+++ POO/Aluno/main.py
@@ -0,0 +1,7 @@
+from Aluno import Aluno
+
+aluno1= Aluno(20145620043,'Martim Abuquerque Fernando',[8.7,9.8,10])
+print(aluno1)
+print(aluno1.CalcularMedia())
+aluno1.AdicionarNota(10)
+print(aluno1)
---
+++ POO/Class/main.py
@@ -0,0 +1,5 @@
+from CLASSES import Pessoa
+
+p1 = Pessoa("anderson" , 19)
+
+print(p1)
