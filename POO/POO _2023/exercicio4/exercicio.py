cf6478e753f9440bfa8b4bf068c8e8391c36c6d0
unknown <anderson.lima18pb@gmail.com>
2023-08-26 15:19:22 -0300

    exercicio_videogame

---
+++ POO/POO _2023/exercicio4/exercicio.py
@@ -0,0 +1,19 @@
+class VideoGame:
+    # a partir que os objetos são criados, é adcionado mais um ao contador
+	contador = 0
+
+	def __init__(self, marca="Sony", modelo="PS5", data_fabricacao=None):
+		self.marca: str = marca
+		self.modelo: str = modelo
+		self.data_fabricacao: str = data_fabricacao if data_fabricacao else "25/08/2023"
+		VideoGame.contador += 1
+		self.numero_serie: int = VideoGame.contador
+		self.capacidade: int = 1024 # MB
+		self.jogos_instalados = ["Overwatch", "Fifa", "Fortnite", "GTA"]
+
+
+
+v1 = VideoGame()
+v2 = VideoGame("10/07/2020")
+v3 = VideoGame(marca="Micro$oft", modelo="XBox", data_fabricacao="01/04/1980")
+print(v1.numero_serie)
