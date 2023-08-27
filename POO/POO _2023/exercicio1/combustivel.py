0b64da625d639ed1a81ba4b24c1c98f330308a6a
unknown <anderson.lima18pb@gmail.com>
2023-08-27 15:28:34 -0300

    combustivel

--- POO/POO _2023/exercicio1/combustivel.py
+++ POO/POO _2023/exercicio1/combustivel.py
@@ -1,22 +1,26 @@
+valor_total = 0
+gasolina = 6
+alcool = 5
+
 class Bomba_combustivel:
-  valor_total = 0
-  valor_gasolina = 6
-  valor_alcool = 5
-  def __init__(self,litro,combustivel=True):
+  
+  def __init__(self,litro,tipo_combustivel):
     self.litro = litro
+    self.tipo_combustivel = tipo_combustivel
     
 
-  def abastecer(self,litros,combustivel,valor_gasolina,valor_alcool):
-    if combustivel == True:
-      valor_total = valor_gasolina * litros
+  def abastecer(self,litros,tipo_combustivel):
+    if tipo_combustivel == gasolina:
+      valor_total = gasolina * litros
       print(f" o valor a ser pago com gasolina é {valor_total} para {litros} litros")
-    valor_total = valor_alcool * litros
-    print(f" o valor a ser pago com alcool é {valor_total} para {litros} litros")
+    else:
+      valor_total = alcool * litros
+      print(f" o valor a ser pago com alcool é {valor_total} para {litros} litros")
     
 
 
 
 
-carro1 = Bomba_combustivel()
-carro1.abastecer(30,True)
+carro1 = Bomba_combustivel(0,gasolina)
+carro1.abastecer(30,gasolina)
 print(carro1)
