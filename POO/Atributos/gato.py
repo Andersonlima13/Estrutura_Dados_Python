83cfad2fbdc3695615cb8e256f51e4ab3957eb63
unknown <anderson.lima18pb@gmail.com>
2023-09-03 15:40:44 -0300

    herança

--- POO/Atributos/gato.py
+++ POO/Atributos/gato.py
@@ -1,10 +1,20 @@
-class Gato:
-    tipo_animal = 'FELINO'
+class Animal:
+    def __init__(self,tipo,idade,nome):
+        self.tipo = tipo
+        self.idade = idade
+        self.nome = nome
+
+
+
+#gato herda a classe animal
+
+class Gato(Animal):
 # PS :  atributos sempre devem iniciar-se com letras minusculas
     def __init__(self,nome,cor,idade,comendo=False,dormindo=True):
+        self.tipo = "felino"
         self.nome = nome
-        self.cor = cor
         self.idade = idade
+        self.cor = cor
         self.comendo = comendo  
         self.dormindo = dormindo
     
@@ -26,7 +36,7 @@ class Gato:
 
     
 g1 = Gato("japodhi_almossar_da_silva","branco","2")
-g2 = Gato("al_mosso","preto","1")
+g2 = Gato("aumosso","preto","1")
 g1.comer("ração")
 g1.dormir()
 
