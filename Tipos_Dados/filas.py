0981cbb6d238adcfb305b62a8324a8a355530c1e
unknown <anderson.lima18pb@gmail.com>
2023-10-15 21:57:48 -0300

    BUG FIX | ADD QUEUE

--- Tipos_Dados/filas.py
+++ Tipos_Dados/filas.py
@@ -2,19 +2,70 @@ class Node:
 	def __init__(self,data):
 		self.data = data
 		self.next = None
+  
+  
+# O PRIMEIRO ELEMENTO A ENTRAR É O PRIMEIRO A SAIR 
+# INSERIMOS NO FINAL E REMOVEMOS DO INICIO
 
 class Fila:
 	def __init__(self):
 		self.first = None
 		self.last = None
 		self._size = 0
-
-	def push(self,elem):
+	
+	def index(self,elem):
 		pass
+ 
+	
+	def remove(self):
+		if self.first is not None:
+			elem = self.first.data
+			self.first = self.first.next
+			self._size = self._size - 1
+			return elem
+		raise IndexError("A fila está vazia")
+
+	
+	def insert(self,elem):
+		node = Node(elem)
+		if self.last is None:
+			self.last = node
+		else:							 # CASO JA TENHA ELEMENTO NA POSIÇÃO LAST , FAREMOS O PROXIMO DO ULTIMO ELEMENTO
+			self.last.next = node		 # SABER QUE UM NOVO NODE FOI ADCIONADO , RECEBENDO ESSE VALOR, ASSIIM SELF.LAST
+			self.last = node			 # PASSA A SER O ELEMENTO QUE ACABOU DE CHEGAR 
+		if self.first is None:
+			self.first = node
+		self._size = self._size + 1
+  
+	def __repr__(self) -> str:
+		string = ""
+		pointer = self.first
+		while(pointer):
+			string = string + str(f" [{pointer.data}]") + "|"
+			pointer = pointer.next
+		return string
+   
+	def modify(self,elem):
+		pass
+
+	def impress(self):
+		return self.__repr__()
 
 
 	def __len__(self):
 		return self._size
 
-	def empty(self):
+	def is_empty(self):
 		return len(self) == 0
+
+
+fila = Fila()
+fila.insert(4)
+fila.insert("anderson")
+fila.insert(False)
+print(fila.impress())
+print(fila.__len__())
+fila.remove()
+print(fila.impress())
+fila.remove()
+print(fila.impress())
--- Tipos_Dados/pilha_imp.py
+++ Tipos_Dados/pilha_imp.py
@@ -3,7 +3,7 @@ class Node:
         self.data = data
         self.next = None
 
-
+# O ULTIMO ITEM A ENTRAR É O PRIMEIRO A SAIR
 
 class Pilha:
     
@@ -12,31 +12,31 @@ class Pilha:
         self.top = None
         self.size = 0
 
-
+# TESTA SE A FILA ESTÁ OU NÃO VAZIA
     def esta_vazia(self):
-        if self.size == 0:
-            return True
-        else:
-            return False
+        return self.size == 0
         
-
+# VERIFICA O TAMANHO DA PILHA
     def tamanho(self):
         return self.__len__()
 
+# INSERE ELEMENTOS NA PILHA
+
     def empilhar(self, elemento):
+        node = Node(elemento)
         if self.top:
-            new_top = Node(elemento)
+            new_top = node
             new_top.next = self.top
             self.top = new_top
         else:
-            self.top = Node(elemento)
+            self.top = node
         self.size = self.size + 1
             
-
+# MÉTODO ESPECIAL PARA VERIFICAR O TAMANHO
     def __len__(self):
         return self.size
         
-
+# REMOVER ELEMENTO DA PILHA
     def desempilhar(self):
         if self.top:
             pointer = self.top
@@ -44,6 +44,8 @@ class Pilha:
             self.size = self.size - 1
         else:
             raise IndexError("Não existem elementos para remover")
+            
+# IMPRIMIR A FORMA DE PIILHA    
             
     def imprimir(self):
         string = ""
@@ -55,10 +57,28 @@ class Pilha:
             
     def __str__(self):
         return self.imprimir()
+  
+
+# ESVAZIAR A PILHA TOTALMENTE    
+
+    def esvaziar_pilha(self):
+        self.top = None
+        self.size = 0
+
+# RETORNAR O ELEMENTO DO TOPO
+
+    def retorna_topo(self):
+        if self.top:
+            return self.top.data       
 
 pilha = Pilha()
 pilha.empilhar(8)
 pilha.empilhar(9)
 pilha.empilhar(10)
+pilha.esvaziar_pilha()
+pilha.empilhar(10)
+pilha.empilhar(14)
+pilha.empilhar(11)
+print(pilha.retorna_topo())
+print(pilha.esta_vazia())
 print(pilha.imprimir())
-pilha.esta_vazia()
