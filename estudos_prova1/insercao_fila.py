bb7bd03eb8609b3f2db3ae0443293f5da1af7b8e
unknown <anderson.lima18pb@gmail.com>
2023-10-20 22:02:14 -0300

    prova

---
+++ estudos_prova1/insercao_fila.py
@@ -0,0 +1,46 @@
+# fila encadeada 
+# sempre inserimos no final , e retiramos no começo
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):	
+        self.first = None
+        self.last = None
+        self.size = 0
+
+    def insert(self,elem):
+        node = Node(elem)
+        if self.last is None:
+            self.last = node
+        else:
+            self.last.next = node
+            self.last = node      
+        if self.first is None:
+            self.first = node 
+        self.size = self.size + 1
+        
+
+
+
+    def imprimir(self):
+        string = ""
+        pointer = self.first
+        while(pointer):
+            string = string + str(pointer.data) + "->"
+            pointer = pointer.next
+        return string
+    
+    
+fila = LinkedList()
+fila.insert(5)
+fila.insert(6)
+fila.insert(7)
+fila.insert(8)
+fila.insert(9)
+fila.insert(10)
+print(fila.imprimir())
