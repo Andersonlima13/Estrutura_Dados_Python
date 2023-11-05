ee246acb59000ec9db3c6d08da5acdc274f660f6
unknown <anderson.lima18pb@gmail.com>
2023-11-05 15:30:05 -0300

    inicio-arvores

---
+++ arvores/avoreBinarias.py
@@ -0,0 +1,17 @@
+# DEFINIMOS O NÓ DA ARVORE , O QUAL CORRE A APENAS DUAS DIREÇÕES
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = left
+        self.right = right
+        
+    def __str__(self):
+        return str(self.data)
+
+class BinaryTree:
+    def __init__(self,data):
+        node = Node(data)
+        self.root = node
+        
+        
