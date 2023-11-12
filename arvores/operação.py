e2cc1392758cf2c94c8774ca36ff4c7d5f7dc990
unknown <anderson.lima18pb@gmail.com>
2023-11-12 17:36:49 -0300

    percorrendo arvores

---
+++ arvores/operação.py
@@ -0,0 +1,20 @@
+from avoreBinarias import BinaryTree, Node
+
+# construindo uma operação matematica com arvores 
+# começará sempre da esquerda , então vamos para 
+# o centro (raiz) e operamos com o filho a direita
+
+
+#     "+"
+#    /   \
+#  "A"    "*"
+#        /   \
+#      "B"   "-"
+#           /   \
+#         "/"   "E"
+#        /  \ 
+#      "C"  "D"
+
+#(A + (B * ((C/D) - E)))
+
+
