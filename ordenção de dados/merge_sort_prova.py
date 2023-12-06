a3c305f71b120597fac438a8bb92cf62e0b59741
Anderson Sousa <anderson.lima18pb@gmail.com>
2023-12-05 21:14:48 -0300

    merge-sort

---
+++ ordenção de dados/merge_sort_prova.py
@@ -0,0 +1,35 @@
+# QUESTÃO DE PROVA ! "MERGE = DIVIDIR PARA CONQUISTAR"
+# O ALGORITIMO É BASEADO EM DIVIDIR SUA LISTA AO PONTO QUE TRANSFORME
+# UMA LISTA EM VARIAS SUB-LISTAS (COM UM VALOR APENAS)
+# LOGO LISTA = [7,5,1,4,6,4] VIRA = [7] [5] [2] [4] [6] [4]
+
+
+def merge_sort(lista,inicio=0,fim=None):
+    if fim is None:
+        fim = len(lista)
+    if(fim - inicio < 1):
+        meio = (fim + inicio)//2    # DIVIDIMOS AQUI A LISTA
+        merge_sort(lista,inicio,meio)  # UMA LISTA VAI DO INICIO AO MEIO (QUE VAI SER ENTENDIDO COMO FIM
+        merge_sort(lista,meio,fim)     # DEPOIS , DO MEIO ATÉ O FINAL
+        merge(lista,inicio,meio,fim)   # AQUI , IREMOS JUNTAR A LISTA
+        
+
+
+def merge(lista,inicio,meio,fim):
+    left = lista[inicio:meio]
+    right = lista[meio:fim]
+    top_left,top_right = 0,0
+    for i in range(inicio,fim):
+        if top_left >= len(left):
+            lista[i] = right[top_right]
+            top_right = top_right + 1
+        if top_right >= len(right):
+            lista[i] = left[top_left]
+            top_left = top_left + 1
+        if left[top_left] < right[top_right]:
+            lista[i] = left[top_left]
+            top_left =  top_left + 1
+        else:
+            lista[i] = right[top_right]
+            top_right = top_right + 1
+    
