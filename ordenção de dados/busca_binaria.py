a59b30a0cc8cd89dc9f802106ae37034f5a040f1
Anderson Sousa <anderson.lima18pb@gmail.com>
2023-12-17 19:03:26 -0300

    merge

--- ordenção de dados/busca_binaria.py
+++ ordenção de dados/busca_binaria.py
@@ -1,12 +1,12 @@
 def busca_binaria(array,item,inicio=0,fim=None):
-    if fim is None:
-        fim = len(array)-1
-    if inicio <= fim:
-        meio = (inicio + fim)//2
-        if array[meio] == item:
-            return meio
-        if item < array[meio]:
-            return busca_binaria(array,item,inicio,meio-1)
+    if fim is None:   # fazemos a verificação do 
+        fim = len(array)-1 
+    if inicio <= fim:    # se a sublista é valida , vamos calcular o meio
+        meio = (inicio + fim)//2  # logo o meio será o valor de inicio + fim / 2
+        if array[meio] == item: # primeira verificação , se o item do meio for o que estamos procurando
+            return meio             # retornamos
+        if item < array[meio]:   # se não , faremos a busca pela esquerda 
+            return busca_binaria(array,item,inicio,meio-1)  # aqui , o fim será o começo -1
         else:
-            return busca_binaria(array,item,meio+1,fim)
+            return busca_binaria(array,item,meio+1,fim)  # enquanto aqui , modificamos o inicio, começado pelo meio +1
     return None
--- ordenção de dados/merge_sort_prova.py
+++ ordenção de dados/merge_sort_prova.py
@@ -7,7 +7,7 @@
 def merge_sort(lista,inicio=0,fim=None):
     if fim is None:
         fim = len(lista)
-    if(fim - inicio < 1):
+    if(fim - inicio < 1): # problema começa aqui , visto que o merge so funciona com elementos iguais a 1
         meio = (fim + inicio)//2    # DIVIDIMOS AQUI A LISTA
         merge_sort(lista,inicio,meio)  # UMA LISTA VAI DO INICIO AO MEIO (QUE VAI SER ENTENDIDO COMO FIM
         merge_sort(lista,meio,fim)     # DEPOIS , DO MEIO ATÉ O FINAL
@@ -15,21 +15,22 @@ def merge_sort(lista,inicio=0,fim=None):
         
 
 
-def merge(lista,inicio,meio,fim):
-    left = lista[inicio:meio] # DIVIDIMOS A LISTA ENTRE ESQUERDA E DIREITA 
-    right = lista[meio:fim]
-    top_left,top_right = 0,0  # ATRIBUIMOS O VALOR DO TOPO INCIANDO COM "0" PARA AMBAS AS LISTAS
-    for i in range(inicio,fim):
-        if top_left >= len(left):
+def merge(lista, inicio, meio, fim):
+    left = lista[inicio:meio]  # Lista da esquerda
+    right = lista[meio:fim]    # Lista da direita
+    top_left, top_right = 0, 0  # Índices para controlar a posição nas listas
+
+    for i in range(inicio, fim):  # Itera sobre a região da lista a ser mesclada
+        if top_left >= len(left):  # Se todos os elementos da lista da esquerda foram usados
             lista[i] = right[top_right]
-            top_right = top_right + 1
-        if top_right >= len(right):
+            top_right += 1
+        elif top_right >= len(right):  # Se todos os elementos da lista da direita foram usados
             lista[i] = left[top_left]
-            top_left = top_left + 1
-        if left[top_left] < right[top_right]:
+            top_left += 1
+        elif left[top_left] < right[top_right]:  # Compara os elementos e mescla ordenadamente
             lista[i] = left[top_left]
-            top_left =  top_left + 1
+            top_left += 1
         else:
             lista[i] = right[top_right]
-            top_right = top_right + 1
+            top_right += 1
     
